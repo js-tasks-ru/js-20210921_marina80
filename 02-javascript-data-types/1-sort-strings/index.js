f0361f01106b1a349d96ff182f419a16290f60a9
extrimarina <marina.goloshchuk@gmail.com>
2021-09-28 15:11:38 +0300

    Update index.js
    sortStrings

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,40 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  const collator = new Intl.Collator(['ru', 'en'], {
+    caseFirst: 'upper'
+  });
+  let arrSort = arr.slice();
 
+  if (param == "asc") {
+    arrSort.sort(function (a, b) {
+      return collator.compare(a, b);
+    });
+  } else {
+    arrSort.sort(function (a, b) {
+      return collator.compare(b, a);
+    });
+  }
+
+  return arrSort;
+  /*  --- or ---
+   let arrSort = arr.slice();
+
+   if (param == "asc") {
+     arrSort.sort(function (a, b) {
+       return a.localeCompare(b, ['ru', 'en'], {
+         caseFirst: 'upper'
+       });
+     });
+   } else {
+     arrSort.sort(function (a, b) {
+       return b.localeCompare(a, ['ru', 'en'], {
+         caseFirst: 'upper'
+       });
+     });
+   }
+
+   return arrSort;
+ */
 }
+
