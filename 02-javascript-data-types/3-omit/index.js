591bc71facb7f61fdeabda7d3018a3992cd72428
extrimarina <marina.goloshchuk@gmail.com>
2021-09-28 15:13:17 +0300

    Update index.js
    omit

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,12 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
+  let objNew = [];
 
+  for (let item of Object.entries(obj)) {
+    if (!fields.includes(item[0])) {
+      objNew.push([item[0], item[1]]);
+    }
+  }
+  return Object.fromEntries(objNew);
 };
