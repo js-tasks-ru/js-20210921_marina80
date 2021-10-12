ff545d2cdd8dfddc00165f10b0b99e5b3d205fdd
extrimarina <marina.goloshchuk@gmail.com>
2021-10-12 15:22:23 +0300

    Update index.js
    Tooltip компонент

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,5 +1,57 @@
 class Tooltip {
 
+  static activeTooltip;
+
+  constructor() {
+    this.initialize();
+  }
+
+  initialize() {
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = '<div class="tooltip"></div>';
+    const element = wrapper.firstElementChild;
+    this.element = element;
+
+    document.addEventListener('pointerover', this.pointerover);
+    document.addEventListener('pointerout', this.pointerout);
+  }
+
+  pointerover = (event) => {
+    if (event.target.dataset.tooltip != undefined) {
+      let message = event.target.getAttribute('data-tooltip');
+      this.element.style.top = event.y + 'px';
+      this.element.style.left = event.x + 'px';
+      this.render(message);
+    }
+  }
+
+  pointerout = (event) => {
+    if (event.target.dataset.tooltip != undefined) {
+      this.destroy();
+    }
+  }
+
+  render(message) {
+    if (!Tooltip.activeTooltip) {
+      Tooltip.activeTooltip = this;
+      //let tooltip = new Tooltip();
+      this.initialize();
+      this.element.append(message);
+      document.body.append(this.element);
+    }
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+    Tooltip.activeTooltip = null;
+  }
 }
 
 export default Tooltip;
